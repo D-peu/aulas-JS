0c251fe5bf82f42bc2e45b0fcf8962b833369b46
Pedro <pedro.dpaula67@gmail.com>
2023-10-05 15:29:32 -0300

    estrutura de dados

---
+++ js/02-estrutura.js
@@ -0,0 +1,11 @@
+/* Estruturas de Dados 
+
+ - Arrays (Vetores/Matrizes)
+ Lista de dados indexados, sequenciais e acessíveis por um índice numérico.
+ 
+  - Objestos
+ Lista de dados não indexados e formados por um propriedades 
+ e valores */
+
+//  Arrays 
+let cursos = ["Node.js", "React", "SQL", "UX/UI Design"];
