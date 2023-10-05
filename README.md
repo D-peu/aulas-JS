185732fbe1c39f33a48984e54fe2ec5fc159c9e0
Pedro <pedro.dpaula67@gmail.com>
2023-10-05 15:03:25 -0300

    js

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# aulas-JS
- Atividades voltadas para linguagem de JavaScript
+# Atividades com linguagem JavaScript
+
+## Sobre a linguagem
+
+- JavaScript NÃO É JAVA!
+- JavaScript pode ser abreviado como JS
+- É uma linguagem voltada principalmente para o Front-End
+- Mas também pode ser usado no Back-End através do Node.js
+- JS pode ser usado para criar recursos interativos nos sites, como por exemplo:
+    - Galeria de fotos 
+    - carrosel/slider de conteúdo 
+    - funcionalidade de navegação 
+    - validações mais avançadas de formulário
+- Js pode ser usado até mesmo para criação de aplicativos e games.
---
+++ js/01-sintaxe.js
@@ -0,0 +1,31 @@
+console.log("Programador Web");
+console.log("Senac Penha")
+
+/* Comentário - ALT SHIFT A */
+// comentário - CTRL ;
+
+/* Manipula dados */
+
+// Variáveis 
+let curso = "HTML e CSS3";
+let ano = 2023
+
+// Constantes
+const mediaMinima = 7;
+
+// Saída de dados a partir da mamória
+console.log(curso);
+console.log(mediaMinima);
+console.log(ano);
+
+
+// Saída personalizada 
+
+/* Estou fazendo o curso de  HTML5 e CSS3 
+neste ano de 2023 */
+
+// Template String/Literal
+console.log(`Estou fazendo curso de ${curso} neste ano de ${ano}.`);
+
+// Concatecação (forma mais antiga de saída, mas ainda válida)
+console.log("Estou fazendo o curso de "+curso+" neste ano de "+ano+".");
