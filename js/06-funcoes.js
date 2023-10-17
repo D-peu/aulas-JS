aa0ebc5ebf2fbf1cb3976c8ed3aaa9359d2dbc8d
Pedro <pedro.dpaula67@gmail.com>
2023-10-17 14:53:41 -0300

    Update 06-funcoes.js

---
+++ js/06-funcoes.js
@@ -0,0 +1,10 @@
+"use strict"
+
+/* Sobre funções
+
+Funções são blocos de código que podem ser executados e reaproveitados em praticamente qualquer lugar de uma aplicação/site.
+
+Ussar funções também é benefico do ponto de vista de organização de código.
+
+O JS já possui centenas de funções prontas:
+Exemplos: log(), alert(), toFixed(2) e etc */
