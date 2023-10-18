ada8eab5c8166036fdc7ff5f066803d26b77c307
Pedro <pedro.dpaula67@gmail.com>
2023-10-18 16:02:49 -0300

    tipos de DOM

---
+++ js/07-dom-eventos.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/* Funções de seleção no DOM
+
+-getElementById()
+Seleciona UM elemento através de um id.
+
+-querySelector()
+Seleciona UM elemento através de um seletor.
+
+-querySelectorAll()
+seleciona VÁRIOS elementos através de um/vários seletor(es)*/
+
+/* Exemplos */
